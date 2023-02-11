d03e9d0a6755bd29a48134d0ad32c6477f96dd92
Sedrak Adoyan <set.adoyan@gmail.com>
2023-02-11 15:28:20 +0400

    feat: add profile, news & 404 pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import Login from './pages/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Login } from './pages/Login'
+import { News } from './pages/News'
+import { Profile } from './pages/Profile'
+import { NotFount } from './pages/NotFound/NotFound'
 
 function App() {
-  return <Login />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<NotFount />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
